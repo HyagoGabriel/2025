aee94bbf3f77ae6d53eefae2435a5d96f3169025
HyagoGabriel <hyagogabriel3@gmail.com>
2025-02-19 11:19:48 -0300

    19

---
+++ 19_02/projeto 19_02/atividade/src/axios/axios.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const api = axios.create({
+    
+    baseURL:"http://localhost:5000/api/v1/",
+    headers:{
+        'accept':'application/json'
+    }
+
+
+});
+
+const sheets = {
+    getEvento:()=>api.get("evento/")
+}
+
+export default sheets;
